a696b29f378cf5b4dec2962c395b647b889928c7
c252 <c252@gmail.com>
2019-01-29 10:33:07 -0500

    it werks

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+#Cyrus Burt |JAN 2019| MIT LICENSE
+
 import perceptron
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
 
-inputs = [1.0,0.75]
+model = perceptron.perceptron(0.1) #initialize a new perceptron with a learning rate of 0.1
 
-neuron = perceptron.perceptron(0.1)
+data = pd.read_csv("iris.data") #read iris csv
 
-for i in range(4):
-    print(neuron.pred(inputs))
-    print(neuron.weights)
-    neuron.train(inputs, 1.0)
+species = data.iloc[0:, 4] #initialize a new array of the iris species, these will get encoded
+
+encoded_species = []
+
+for i in species:
+    if i == "Iris-setosa":
+        encoded_species.append(1)
+    if i == "Iris-versicolor":
+        encoded_species.append(0)
+    if i == "Iris-virginica":
+        encoded_species.append(-1)
+
+
+plt.plot(data.iloc[0:49, 2], data.iloc[0:49, 0], linestyle="None", marker=".")
+plt.plot(data.iloc[49:100, 2], data.iloc[49:100, 0], linestyle="None", marker=".")
+plt.plot(data.iloc[100:, 2], data.iloc[100:, 0], linestyle="None", marker=".")
+
+# plt.plot(data.iloc[0:49, 2], encoded_species[0:49], linestyle="None", marker=".")
+# plt.plot(data.iloc[49:100, 2], encoded_species[49:100], linestyle="None", marker=".")
+# plt.plot(data.iloc[100:, 2], encoded_species[100:], linestyle="None", marker=".")
+
+plt.show()
