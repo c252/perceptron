56fe51e4b3e270e19c0ad0ad51401e3d095b543e
c252 <c252@gmail.com>
2019-01-29 17:05:49 -0500

    g

--- main.py
+++ main.py
@@ -4,30 +4,27 @@ import perceptron
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
+from sklearn.utils import shuffle
 
-model = perceptron.perceptron(0.1) #initialize a new perceptron with a learning rate of 0.1
+model = perceptron.perceptron(0.5) #initialize a new perceptron with a learning rate of 0.1
 
 data = pd.read_csv("iris.data") #read iris csv
 
-species = data.iloc[0:, 4] #initialize a new array of the iris species, these will get encoded
+data = shuffle(data)
+
+species = list(data.iloc[0:, 4]) #initialize a new array of the iris species, these will get encoded
+
+petal_length = list(data.iloc[0:, 2]) 
 
 encoded_species = []
 
 for i in species:
     if i == "Iris-setosa":
-        encoded_species.append(1)
+        encoded_species.append(1.0)
     if i == "Iris-versicolor":
-        encoded_species.append(0)
-    if i == "Iris-virginica":
-        encoded_species.append(-1)
+        encoded_species.append(-1.0)
 
+for i in range(len(species)):
+    model.train(petal_length[i], encoded_species[i])
 
-plt.plot(data.iloc[0:49, 2], data.iloc[0:49, 0], linestyle="None", marker=".")
-plt.plot(data.iloc[49:100, 2], data.iloc[49:100, 0], linestyle="None", marker=".")
-plt.plot(data.iloc[100:, 2], data.iloc[100:, 0], linestyle="None", marker=".")
-
-# plt.plot(data.iloc[0:49, 2], encoded_species[0:49], linestyle="None", marker=".")
-# plt.plot(data.iloc[49:100, 2], encoded_species[49:100], linestyle="None", marker=".")
-# plt.plot(data.iloc[100:, 2], encoded_species[100:], linestyle="None", marker=".")
-
-plt.show()
+print(model.pred(1.4))
--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,5 @@
+#Cyrus Burt |JAN 2019| MIT LICENSE
+
 import random
 
 #like all perceptrons this can only classify 2d linearly separable data
@@ -6,21 +8,21 @@ class perceptron(object):
   
   def __init__(self, lr): #takes 1d vector for inputs and learning rate
     self.lr = lr
-    self.weights = [0]*2 #intializes the weights for the perceptron, there will always be two 
+    self.weights = [0.0]*2 #intializes the weights for the perceptron, there will always be two 
 
     for i in range(len(self.weights)): #give the weights of the inputs a random value from -1 to 1
       self.weights[i] = random.uniform(-1,1)
 
   def activation(self, val): # simple linear activation function
-    if val > 0:
-      return 1
+    if val > 0.0:
+      return 1.0
     else:
-      return -1
+      return -1.0
 
   def pred(self,inputs):
-    sum = 0
+    sum = 0.0
     for i in range(len(self.weights)): #caculate the weighted sum
-      sum += inputs[i] * self.weights[i]
+      sum += inputs * self.weights[i]
 
     return self.activation(sum)
 
@@ -29,4 +31,4 @@ class perceptron(object):
     error = ans - guess
 
     for i in range(len(self.weights)):
-      self.weights[i] += self.lr * error * inputs[i]
+      self.weights[i] += self.lr * error * inputs
