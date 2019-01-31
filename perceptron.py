b8d39bb3845522ecf4bffc3765069760cab91a07
c252 <cyrusb252@gmail.com>
2019-01-30 21:34:58 -0500

    eh

--- perceptron.py
+++ perceptron.py
@@ -8,10 +8,10 @@ class perceptron(object):
   
   def __init__(self, lr): #takes 1d vector for inputs and learning rate
     self.lr = lr
-    self.weights = [0.0]*2 #intializes the weights for the perceptron, there will always be two 
+    self.weights = [] #intializes the weights for the perceptron, there will always be two 
 
-    for i in range(len(self.weights)): #give the weights of the inputs a random value from -1 to 1
-      self.weights[i] = random.uniform(-1,1)
+    for i in range(2): #give the weights of the inputs a random value from -1 to 1
+      self.weights.append(random.uniform(-1,1))
 
   def activation(self, val): # simple linear activation function
     if val > 0.0:
