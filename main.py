287df5df502e8775c51b188cb77a01d3abecebdc
Cyrus B <cyrusb252@gmail.com>
2019-02-05 10:27:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #Cyrus Burt |JAN 2019| MIT LICENSE
 
+"""
+main.py
+uses my simple perceptron to classify irises
+
+>>> model.pred([1.4,0.2])
+1.0
+>>> model.pred([4.5,1.6])
+-1.0
+"""
 import perceptron
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
 from sklearn.utils import shuffle
 
-model = perceptron.perceptron(0.01) #initialize a new perceptron with a learning rate of 0.1
+model = perceptron.perceptron(0.5) #initialize a new perceptron with a learning rate of 0.1
 
 data = pd.read_csv("iris.data") #read iris csv
 
@@ -25,8 +34,17 @@ for i in species:
     if i == "Iris-versicolor":
         encoded_species.append(-1.0)
 
-for i in range(len(encoded_species)):
-    model.train([petal_length[i], petal_width[i]], encoded_species[i])
+for _ in range(100):
+    for i in range(len(encoded_species)):
+        model.train([petal_length[i], petal_width[i]], encoded_species[i])
 
-print(model.pred([1.4,0.2]))#should be 1
-print(model.pred([4.5,1.6]))#should be -1
+def classify_iris(petal_l, petal_w):
+    if model.pred([petal_l, petal_w]) == 1:
+        return "Iris-setosa"
+
+    else:
+        return "Iris-versicolor"
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
